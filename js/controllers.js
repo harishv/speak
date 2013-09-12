d14d915153b55ba6f79c54c721441a1405fc0d32
Harish Varada <harish.varada@gmail.com>
2013-09-12 16:08:34 +0530

    Updated the public with live site changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,110 +1,202 @@
-function FVideo($scope, $http, $window, $location) {
+var app = angular.module('speakglobalApp', ['ui.bootstrap']);
+
+app.factory('speakglobalHomePageService', function ($http) {
+	return {
+		getVideosCount: function () {
+			//since $http.get returns a promise,
+			//and promise.then() also returns a promise
+			//that resolves to whatever value is returned in it's
+			//callback argument, we can return that.
+			return $http.get('http://speakglobally.net/api/videos/count').then(function(result) {
+				return result.data.rows;
+			});
+		},
+		getNoneFeaturedVideos: function (videosPerPage, skipValue) {
+			return $http.get('http://speakglobally.net/api/videos/featured_none?limit=' + videosPerPage + '&skip=' + skipValue).then(function(result) {
+				return result.data.rows;
+			});
+		},
+		getLatestVideos: function () {
+			return $http.get('http://speakglobally.net/api/videos/latest').then(function (result) {
+				return result.data.rows;
+			});
+		},
+		getHomeVideo: function (){
+			return $http.get('http://speakglobally.net/api/videos/home_video').then(function (result){
+				// Pick a random number from 15 and get that video from Top 15 videos sorted in order.
+				var randVideoCount = Math.floor((Math.random() * 15) + 1);
+				return result.data.rows[randVideoCount];
+			});
+		}
+	};
+});
+
+// Single Video Page Serives
+app.factory('speakglobalVideoPageService', function ($http) {
+	return {
+		getVideoDetails: function (vId){
+			return $http.get('http://speakglobally.net/api/videos/single_video/' + vId).then(function (result){
+				return result.data;
+			});
+		}
+	};
+});
+
+// Similar Videos Page Serives
+app.factory('speakglobalSimilarVideosPageService', function ($http) {
+	return {
+		getSimilarVideosCount: function () {
+			return $http.get('http://speakglobally.net/api/videos/count').then(function(result) {
+				return result.data.rows;
+			});
+		},
+		getSimilarVideos: function (videosPerPage, skipValue) {
+			return $http.get('http://speakglobally.net/api/videos/featured_none?limit=' + videosPerPage + '&skip=' + skipValue).then(function(result) {
+				return result.data.rows;
+			});
+		},
+		getLatestVideos: function () {
+			return $http.get('http://speakglobally.net/api/videos/latest').then(function (result) {
+				return result.data.rows;
+			});
+		},
+	};
+});
+
+// Home Page Controller
+app.controller('SpeakglobalHome', function ($scope, speakglobalHomePageService, $window, $log) {
 
 	// Non Featured Videos i.e all Videos
 	$scope.videosPerPage = 42;
 
 	// Javascript Custom Function to get teh URL params, decode them
-	function getURLParameter(name) {
+	function getURLParameter (name) {
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 	}
 
-	// Get the Video Count
-	$http.get('http://speakglobally.net/api/videos/count').success(function (vidCount) {
-		$scope.videoCount = vidCount.rows[0].value;
-		$scope.numberOfPages = (Math.ceil($scope.videoCount/$scope.videosPerPage)).toString();
-		$scope.pages = [];
-		var i;
-		for(i=1;i<=$scope.numberOfPages;i++){
-			$scope.pages.push(i);
+	// Get all Video's Count
+	$scope.videoCount = speakglobalHomePageService.getVideosCount();
+	// Generate the numberOfPages and pages content based on the videoCount
+	$scope.$watch('videoCount', function(videoCountObj) {
+		if (videoCountObj !== undefined) {
+			// Sample Output: {"rows":[{"key":null,"value":650}]}
+			$scope.numberOfPages = (Math.ceil(videoCountObj[0].value/$scope.videosPerPage)).toString();
+
+			// Pagination plugin
+			$scope.bigTotalItems = videoCountObj[0].value;
 		}
-
 	});
 
-	var skipValue = 0;
-	var urlToQuery = '';
-	if (location.pathname == '/'){
+	// Get noneFeaturedVideos list based on the page(number) we are hitting from.
+	$scope.currentPageNumber = parseInt(getURLParameter('p'), 10);
+	if (isNaN($scope.currentPageNumber)) {
 		skipValue = 0;
-		urlToQuery = 'http://speakglobally.net/api/videos/featured_none?limit=' + $scope.videosPerPage + '&skip=0';
-	} else{
-
-		$scope.currentPageNumber = parseInt(getURLParameter('p'), 10);
-		skipValue = ($scope.currentPageNumber - 1) * $scope.videosPerPage;
-		urlToQuery = 'http://speakglobally.net/api/videos/featured_none?limit=' + $scope.videosPerPage + '&skip=' + skipValue;
+		$scope.currentPageNumber = 1;
+	} else {
+		skipValue = parseInt(($scope.currentPageNumber - 1) * $scope.videosPerPage, 10);
 	}
-	$http.get(urlToQuery).success(function(data3) {
-		$scope.noneFeaturedVideos = data3.rows;
-	});
+	$scope.noneFeaturedVideos = speakglobalHomePageService.getNoneFeaturedVideos($scope.videosPerPage, skipValue);
+
+	// Pagination plugin
+	$scope.bigCurrentPage = $scope.currentPageNumber;
+	$scope.maxSize = 6; // Max number of pages to be displayed at a time
 
 
-	// Latest Videos
-	$http.get('http://speakglobally.net/api/videos/latest').success(function(data4) {
-		$scope.latestVideos = data4.rows;
-	});
-
-	$http.get('http://speakglobally.net/api/videos/home_video').success(function(dat) {
-		// Get a random number from 15. Top 15 videos sorted in order
-		var i = Math.floor((Math.random()*15)+1);
-		$scope.homeVideoEmbedPath = dat.rows[i].value.video_path;
-		$scope.homeVideoTitle = dat.rows[i].value.title;
-		$window.hiro.playList[0].url= "http://91cefb89b61292d7a6a5-9b3e53ad93e76fa27450765a72dfcdf1.r61.cf2.rackcdn.com/" + dat.rows[i].value.video_path;
-		$window.hiro.playList[0].customProperties.videoTitle = dat.rows[i].value.title;
-		$window.hiro.playList[0].customProperties.videoExternalId = dat.rows[i].value._id;
-		$window.hiro.playList[0].customProperties.videoDescription = dat.rows[i].value.description;
-		$window.hiro.playList[0].customProperties.videoKeyWords = dat.rows[i].value.description;
-		$window.hiro.playList[0].customProperties.videoTags = dat.rows[i].value.title;
-		$window.hiro.playList[0].customProperties.videoDurationSecs = dat.rows[i].value.duration;
-
-	});
-}
-
-// Inidividual Video
-function IVideo($scope, $http, $window){
-
-
-	var qUrl="";
-	$scope.init = function(id){
-		$scope.video_id = id;
-		qUrl = 'http://speakglobally.net/api/videos/single_video/' + $scope.video_id;
-		// alert($scope.video_id);
-			$http.get(qUrl ).success(function(dat) {
-				$scope.homeVideoEmbedPath = dat.video_path;
-				$scope.homeVideoTitle = dat.title;
-				$window.hiro.playList[0].url= "http://91cefb89b61292d7a6a5-9b3e53ad93e76fa27450765a72dfcdf1.r61.cf2.rackcdn.com/" + dat.video_path;
-				$window.hiro.playList[0].customProperties.videoTitle = dat.title;
-				$window.hiro.playList[0].customProperties.videoExternalId = dat._id;
-				$window.hiro.playList[0].customProperties.videoDescription = dat.description;
-				$window.hiro.playList[0].customProperties.videoKeyWords = dat.description;
-				$window.hiro.playList[0].customProperties.videoTags = dat.title;
-				$window.hiro.playList[0].customProperties.videoDurationSecs = dat.duration;
-		});
+	// Pagination plugin
+	// This function is triggred when user tends to change the page using the plugin.
+	$scope.pageChanged = function(page) {
+		location.replace('/home.html?p=' + page);
 	};
 
+	// Get the top 5 Latest Videos List
+	$scope.latestVideos = speakglobalHomePageService.getLatestVideos();
 
-}
+	// Set the homeVideoEmbedPath, homeVideoTitle, hiro player with the video playlist after getting the valid Video's Object
+	$scope.homeVideo = speakglobalHomePageService.getHomeVideo();
+	$scope.$watch('homeVideo', function(homeVideoObj) {
+		if (homeVideoObj !== undefined) {
+			$scope.homeVideoEmbedPath = homeVideoObj.value.video_path;
+			$scope.homeVideoTitle = homeVideoObj.value.title;
 
-function SimilarVideos($scope,$http){
-	$scope.videosPerPage = 42;
-	// Get the count of Videos
-	$http.get('http://speakglobally.net/api/videos/count').success(function(vidCount) {
-		$scope.videoCount = vidCount.rows[0].value;
-		$scope.numberOfPages = (Math.ceil($scope.videoCount/$scope.videosPerPage)).toString();
-		$scope.pages = [];
-		var i;
-		for(i=1;i<=$scope.numberOfPages;i++){
-			$scope.pages.push(i);
+			$window.hiro.playList[0].url= "http://91cefb89b61292d7a6a5-9b3e53ad93e76fa27450765a72dfcdf1.r61.cf2.rackcdn.com/" + homeVideoObj.value.video_path;
+			$window.hiro.playList[0].customProperties.videoTitle = homeVideoObj.value.title;
+			$window.hiro.playList[0].customProperties.videoExternalId = homeVideoObj.value._id;
+			$window.hiro.playList[0].customProperties.videoDescription = homeVideoObj.value.description;
+			$window.hiro.playList[0].customProperties.videoKeyWords = homeVideoObj.value.description;
+			$window.hiro.playList[0].customProperties.videoTags = homeVideoObj.value.title;
+			$window.hiro.playList[0].customProperties.videoDurationSecs = homeVideoObj.value.duration;
 		}
-
 	});
 
-	var urlToQuery = 'http://speakglobally.net/api/videos/featured_none?limit=' + $scope.videosPerPage + '&skip=0';
+});
 
-	$http.get(urlToQuery).success(function(data5) {
-		$scope.noneFeaturedVideos = data5.rows;
+// Single Video Page Controller
+app.controller('SpeakglobalSingleVideo', function ($scope, speakglobalVideoPageService, $window, $log) {
+	// Fetch the Video Information by transfering the Video ID from the template.
+	$scope.init = function (vId){
+		$scope.video_id = vId;
+		$scope.videoDetails = speakglobalVideoPageService.getVideoDetails(vId);
+		// Set the homeVideoEmbedPath, homeVideoTitle, hiro player with the video playlist after getting the valid Video Object
+		$scope.$watch('videoDetails', function(videoObj) {
+			if (videoObj !== undefined) {
+				$scope.homeVideoEmbedPath = videoObj.video_path;
+				$scope.homeVideoTitle = videoObj.title;
+				$window.hiro.playList[0].url= "http://91cefb89b61292d7a6a5-9b3e53ad93e76fa27450765a72dfcdf1.r61.cf2.rackcdn.com/" + videoObj.video_path;
+				$window.hiro.playList[0].customProperties.videoTitle = videoObj.title;
+				$window.hiro.playList[0].customProperties.videoExternalId = videoObj._id;
+				$window.hiro.playList[0].customProperties.videoDescription = videoObj.description;
+				$window.hiro.playList[0].customProperties.videoKeyWords = videoObj.description;
+				$window.hiro.playList[0].customProperties.videoTags = videoObj.title;
+				$window.hiro.playList[0].customProperties.videoDurationSecs = videoObj.duration;
+			}
+		});
+	};
+});
+
+// Similar Video's Page Controller
+app.controller('SpeakglobalSimilarVideos', function ($scope, speakglobalSimilarVideosPageService, $window, $log) {
+	// Similar Videos
+	$scope.videosPerPage = 42;
+
+	// Javascript Custom Function to get teh URL params, decode them
+	function getURLParameter (name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+	}
+
+	// Get all Similar Video's Count
+	$scope.videoCount = speakglobalSimilarVideosPageService.getSimilarVideosCount();
+	// Generate the numberOfPages and pages content based on the videoCount
+	$scope.$watch('videoCount', function(videoCountObj) {
+		if (videoCountObj !== undefined) {
+			// Sample Output: {"rows":[{"key":null,"value":650}]}
+			$scope.numberOfPages = (Math.ceil(videoCountObj[0].value/$scope.videosPerPage)).toString();
+
+			// Pagination plugin
+			$scope.bigTotalItems = videoCountObj[0].value;
+		}
 	});
 
-	$http.get('http://speakglobally.net/api/videos/latest').success(function(data4) {
-		$scope.latestVideos = data4.rows;
-	});
-}
+	// Get Similar videos list based on the page(number) we are hitting from.
+	$scope.currentPageNumber = parseInt(getURLParameter('p'), 10);
+	if (isNaN($scope.currentPageNumber)) {
+		skipValue = 0;
+		$scope.currentPageNumber = 1;
+	} else {
+		skipValue = parseInt(($scope.currentPageNumber - 1) * $scope.videosPerPage, 10);
+	}
+	$scope.similarVideos = speakglobalSimilarVideosPageService.getSimilarVideos($scope.videosPerPage, skipValue);
 
+	// Pagination plugin
+	$scope.bigCurrentPage = $scope.currentPageNumber;
+	$scope.maxSize = 6; // Max number of pages to be displayed at a time
 
+	// Pagination plugin
+	// This function is triggred when user tends to change the page using the plugin.
+	$scope.pageChanged = function (page) {
+		location.replace('/home.html?p=' + page);
+	};
+
+	// Get the top 5 Latest Videos List
+	$scope.latestVideos = speakglobalSimilarVideosPageService.getLatestVideos();
+
+});
